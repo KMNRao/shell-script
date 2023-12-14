43e0b8157a4aa4a4104eb8678bb621b9a5c882b3
KMNRao <kmohanarao09@gmail.com>
2023-12-14 13:55:10 +0530

    code changed

---
+++ shell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=ram
+PERSON2=shiva
+
+echo "$PERSON1: hellow how r u?"
+echo "$PERSON2: fine, how r u?"
+echo "$PERSON1: Are youn practicing devops?"
+echo "$PERSON2: yes i am practing"
