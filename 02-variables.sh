e1e8b7cf31d512f5d923c22576eb075d58e8521b
KMNRao <kmohanarao09@gmail.com>
2023-12-14 15:18:38 +0530

    changed code

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
-echo "ramesh: Hellow suresh,good morning"
-echo "suresh: Hi ramesh,very good"
+PERSON1=Ramesh
+PERSON2=Suresh
+echo "$person1: Hellow $person2,good morning"
+echo "$person2: Hi $person1,very good morning"
+echo "$person1: $person2 where are you going"
+echo "$person2: I am going collage $person1"
