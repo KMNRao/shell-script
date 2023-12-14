364ad71a8bdd5ad2a1570216392ee496f8aa50f1
KMNRao <kmohanarao09@gmail.com>
2023-12-14 16:41:40 +0530

    changed code

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-#NUMBER1=$1
-#NUMBER2=$2
-
-#SUM=$(($NUMBER1+$NUMBER2))
-#echo "Total: $sum"
-
 NUMBER1=$1
 NUMBER2=$2
 SUM=$(($NUMBER1+$NUMBER2))
-echo "Total:: $SUM"
+echo "Total: $sum"
+
+#NUMBER1=$1
+#NUMBER2=$2
+#SUM=$(($NUMBER1+$NUMBER2))
+#echo "Total:: $SUM"
