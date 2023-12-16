3163878dca1bc330ec86557ffc43ddc4ac11e215
KMNRao <kmohanarao09@gmail.com>
2023-12-16 07:38:13 +0530

    changed arrrays

--- 19-lines.sh
+++ 19-lines.sh
@@ -4,4 +4,5 @@ LINE1=I am learning Devops
 LINE2=The tutor was Shiva
 LINE3=I am practicing Devops 
 
-
+awk "Begin{$LINE1=0} // {$LINE1++} end {"print number of lines: " ,$LINE1} $LINE1 $LINE2 $LINE3
+awk "Begin{$LINE1=0} // {$LINE1++} end {"print of words: ",$LINE1} RS="[[:space:]]" $LINE1 $LINE2 $LINE3
