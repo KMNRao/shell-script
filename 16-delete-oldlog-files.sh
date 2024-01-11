8af85d8a9128f3b29d61bb00d0d47a8b874f735a
KMNRao <kmohanarao09@gmail.com>
2024-01-12 05:04:37 +0530

    shell script

--- 16-delete-oldlog-files.sh
+++ 16-delete-oldlog-files.sh
@@ -5,7 +5,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-SOURCE_DIR="/tmp/shellscript-logs"
+SOURCE_DIR="/tmp/shell-script.logs"
 
 if [ ! -d $SOURCE_DIR ] #! denotes opposit, in this case if source dir does not exsist
 then
