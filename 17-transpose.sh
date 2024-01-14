7c7ef2dc4dd53a8dfc7726907ba9b0b137226bb4
KMNRao <kmohanarao09@gmail.com>
2024-01-14 11:34:53 +0530

    shell script

--- 17-transpose.sh
+++ 17-transpose.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-file="file1.txt"
-name=()
-ages=()
-while IFS= read -r line; 
-do
- words=($line)
- name+=(${words[0]})
- ages+=(${words[1]})
-done <"$file"
+# file="file1.txt"
+# name=()
+# ages=()
+# while IFS= read -r line; 
+# do
+#  words=($line)
+#  name+=(${words[0]})
+#  ages+=(${words[1]})
+# done <"$file"
 
-echo "${name[]}"
-echo "${ages[]}"
+# echo "${name[]}"
+# echo "${ages[]}"
+
+input_file="$1"
+num_cols=$(head -1 "$input_file" | wc -w)
+for i in $(seq 1 "$num_cols"); do
+echo $(cut -d ' ' -f "$i" "$input_file")
+done
+
+cat data
+name age
+tanvi 8
+reyan 7
+junnu 6
